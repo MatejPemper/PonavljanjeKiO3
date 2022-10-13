eafe077906bf1a2749e087b8261182a6f58bee43
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-10-13 21:23:53 +0200

    Add files via upload

---
+++ PonavljanjeKiO3/PonavljanjeKiO3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonavljanjeKiO3
+{
+    internal class Program
+    {
+
+        class Vozilo
+        {
+            private bool JeLiLeti, JeLiPlovi;
+
+            public bool JeLiLeti1 { get => JeLiLeti; set => JeLiLeti = value; }
+            public bool JeLiPlovi1 { get => JeLiPlovi; set => JeLiPlovi = value; }
+
+            public string KudaVozi()
+            {
+                return ("U zraku: " + JeLiLeti + " Na moru: " + JeLiPlovi);
+            }
+
+        }
+        class Brod : Vozilo
+        {
+            public Brod()
+            {
+                JeLiLeti1 = false;
+                JeLiPlovi1 = true;
+            }
+        }
+        class Zrakoplov : Vozilo
+        {
+
+            public Zrakoplov()
+            {
+                JeLiLeti1 = true;
+                JeLiPlovi1 = false;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Brod brod = new Brod();
+            Zrakoplov z = new Zrakoplov();
+
+            Console.WriteLine("Brod: " + brod.KudaVozi());
+            Console.WriteLine("Zrakoplov: " + z.KudaVozi());
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
